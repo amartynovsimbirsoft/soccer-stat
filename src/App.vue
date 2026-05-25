6f5560fc7d5a5487d121fcc3302edba1b2919343
Anton Martynov <anton.martynov@simbirsoft.com>
2026-05-25 17:07:20 +0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <MatchCard
+      home-team="Зенит"
+      away-team="Спартак"
+      :home-score="2"
+      :away-score="1"
+      status="FINISHED"
+    />
+    <MatchCard
+      home-team="Реал"
+      away-team="Барселона"
+      :home-score="0"
+      :away-score="0"
+      status="LIVE"
+    />
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import MatchCard from './components/MatchCard.vue'
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
